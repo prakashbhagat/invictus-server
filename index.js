996f55231c7cf8837769fc305d490ca45958ff44
prakashbhagat <54275178+prakashbhagat@users.noreply.github.com>
2022-02-22 15:30:12 +0530

    update with heroku

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 require("./src/routes/student.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
